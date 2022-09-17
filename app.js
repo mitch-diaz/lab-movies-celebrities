20c66e50b27fc6a17da50df5a9f420727a62eb61
Mitchell Diaz <mitchelldiazlmt@gmail.com>
2022-09-17 14:19:40 -0400

    initial project setup

--- app.js
+++ app.js
@@ -28,6 +28,13 @@ app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
 const index = require('./routes/index');
 app.use('/', index);
 
+app.use('/', require('./routes/index'));
+app.use('/celebrities.routes', require('./routes/celebrities.routes'));
+app.use('/movies.routes', require('./routes/movies.routes'));
+
+
+
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
 
---
+++ routes/celebrities.routes.js
@@ -0,0 +1,8 @@
+// starter code in both routes/celebrities.routes.js and routes/movies.routes.js
+const router = require("express").Router();
+
+router.get('/celebrities/create', (req, res, next) => {
+    
+})
+
+module.exports = router;
