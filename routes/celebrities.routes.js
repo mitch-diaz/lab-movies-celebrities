eb66098dfcce7a59f4764e23c321ea9e6d35b141
Mitchell Diaz <mitchelldiazlmt@gmail.com>
2022-09-20 00:03:46 -0400

    added code for iterations 6-9. 8 and 9 are not complete.

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,6 +1,4 @@
 const Celebrity = require("../models/Celebrity.model");
-
-// starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
 
 router.get('/celebrities/create', (req, res, next) => {
@@ -38,8 +36,6 @@ router.get('/celebrities', (req, res, next) => {
         console.log({err});
     })
 })
-
-
 
 
 
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,9 +1,8 @@
-const Celebrity = require("../models/Movie.model");
-
-// starter code in both routes/celebrities.routes.js and routes/movies.routes.js
+const Movie = require("../models/Movie.model");
 const router = require("express").Router();
 
-// all your routes here
+
+// =========== CREATE ============
 router.get('/movies/create', (req, res, next) => {
     res.render('movies/new-movie')
 })
@@ -19,11 +18,68 @@ router.post('/movies/create', (req, res ,next) => {
 
     Movie.create(moviesToCreate)
     .then(newlyCreatedMovie => {
-        res.redirect('/celebrities/list', {newlyCreatedMovie});
+        res.redirect('/movies/movies', {newlyCreatedMovie});
     }).catch(err => {
-        res.redirect('/celebrities/create');
+        res.redirect('/movies/create');
     })
 })
+
+
+// =========== READ ============
+router.get('/movies', (req, res, next) => {
+    Movie.find()
+    .then((moviesFromDb) => {
+        console.log({moviesFromDb});
+        movieData = {movies: moviesFromDb}
+        res.render('movies/movies', movieData);
+    })
+    .catch(err => {
+        console.log({err});
+    })
+})
+
+// *** The only way to get a value from req.params is if you personally set a variable using the :variableName method in the endpoint when creating your route. You then call the value for that parameter by using req.params.variableName
+
+router.get('/movies/:movieId', (req, res, next) => {
+    console.log({params: req.params.movieId})
+
+    Movie.findById(req.params.movieId)
+    .populate('cast')
+    .then(moviesFromDb => {
+        // console.log({moviesFromDb});
+        movieData = {movies: moviesFromDb}
+        res.render('movies/movie-details', moviesFromDb);
+    }).catch(err => {console.log({err})})
+})
+
+// ================
+// router.get('/movies/:movieId', (req, res, next) => {
+//     const {movieId} = req.params;
+//     console.log({params: req.params.movieId})
+
+//     Movie.findById(movieId)
+//     .then(theMovie => {res.render('movies/movie-details', { movie: theMovie });
+//     }).catch(err => {console.log({err})})
+// })
+
+
+// =========== DELETE ============
+
+router.post('/movies/:movieId/delete', (req, res, next) => {
+    const {movieId} = req.params;
+
+    Movie.findByIdAndRemove('movieId')
+    .then(() => {
+        res.redirect('/movies/movies');
+    })
+    .catch((err) => {
+        console.log({err});
+    })
+});
+
+
+// =========== UPDATE ============
+
 
 
 
